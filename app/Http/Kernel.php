96a22fb811f527ab68c60f525ebc4df96464f91a
limmbooo <Ferdyyy720@gmail.com>
2024-11-21 20:34:00 +0700

    SonarScanner Changes

---
+++ app/Http/Kernel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * Global HTTP middleware stack.
+     *
+     * These middleware are run during every request to your application.
+     *
+     * @var array<int, class-string|string>
+     */
+    protected $middleware = [
+        // Middleware kustom untuk logging fuzzing input
+        \App\Http\Middleware\LogFuzzingRequests::class,
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -5,26 +5,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VulnerabilityController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::apiResource('users', UserController::class);
 
 Route::get('/vulnerable-sql', [VulnerabilityController::class, 'sqlInjection']);
 Route::post('/vulnerable-xss', [VulnerabilityController::class, 'xss']);
-
-Route::get('/vulnerable-auth', [VulnerabilityController::class, 'noAuth']);
-Route::get('/vulnerable-directory', [VulnerabilityController::class, 'directoryTraversal']);
 Route::post('/vulnerable-upload', [VulnerabilityController::class, 'insecureFileUpload']);
 
+Route::apiResource('users', UserController::class);
+Route::get('/vulnerable-auth', [VulnerabilityController::class, 'noAuth']);
+Route::get('/vulnerable-directory', [VulnerabilityController::class, 'directoryTraversal']);
 
 
 
